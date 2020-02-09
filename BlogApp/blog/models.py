f13dc2b33dec478d5f0a89692503e0a01f10f5e3
prodanalex99 <prodanalex323@gmail.com>
2020-02-09 19:23:09 +0200

    Added forms

--- BlogApp/blog/models.py
+++ BlogApp/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -10,6 +10,15 @@ class Post(models.Model):
 
     body = models.TextField()
 
+
+    #Good practice:overriding str and get_absolute_url
     def __str__(self):
         return self.title
 
+
+    #For forms => where data is send
+    #Reverse -> reference an object by it's URL template name
+    #post detail in urls.py
+    #so after click on submit in form -> redirect to the detail page of new post
+    def get_absolute_url(self):
+        return reverse('post_detail',args=[str(self.id)]) #ex:return '' -> remain on the same page (with the form)
